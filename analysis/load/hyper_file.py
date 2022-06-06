3dd826196d797f5514ac05d144c5dedb97887333
Selma Catakovic <83902865+Scatalina1@users.noreply.github.com>
2022-06-06 20:32:07 +0200

    Update hyper_file.py

--- analysis/load/hyper_file.py
+++ analysis/load/hyper_file.py
@@ -1,4 +1,3 @@
-
 from tableau_api_lib.utils.querying import get_projects_dataframe
 from tableauhyperapi import NOT_NULLABLE, NULLABLE, HyperProcess, Connection, Nullability, TableDefinition, SqlType, Telemetry, Inserter, CreateMode, TableName
 from tableau_api_lib.utils.querying import get_projects_dataframe
@@ -32,29 +31,34 @@ def create_hyper_file(conn, my_df, project_name):
     # Step 4: Create the table definition
             schema = TableDefinition(table_name=TableName('Extract','Extract'),
                 columns=[
-                TableDefinition.Column('COLUMN_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('COLUMN_NAME', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('UPSTR_DB_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('upstr_db_isEmbedded', SqlType.bool()),
-                TableDefinition.Column('UPSTR_DB_NAME', SqlType.varchar(max_length= 200)),
-                TableDefinition.Column('UPSTR_DB_TYPE', SqlType.text()),
-                TableDefinition.Column('DATABASE_HOSTNAME', SqlType.text(), NULLABLE),
-         #       TableDefinition.Column('DATABASE_PORT', SqlType.varchar(max_length=200), NULLABLE),
-                TableDefinition.Column('EMB_DS_NAME', SqlType.varchar(max_length= 200)),
-                TableDefinition.Column('EMB_DS_REFRESH_TIME', SqlType.varchar(max_length=200)),
-                TableDefinition.Column('FIELD_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('FIELD_NAME',SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('site.name', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('TABLE_NAME', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('UPSTR_TABLE_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('UPSTR_TABLE_NAME', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('VIEW_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('VIEW_NAME', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('VIEW_TYPE', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('WORKBOOK_ID', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('WORKBOOK_NAME', SqlType.varchar(max_length= 200), NOT_NULLABLE),
-                TableDefinition.Column('WORKBOOK_PROJECT', SqlType.varchar(max_length= 200), NOT_NULLABLE)
+                TableDefinition.Column('COLUMN_ID', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('COLUMN_NAME', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('DATABASE.UPSTR_DB_ID', SqlType.varchar(max_length= 500)),
+                TableDefinition.Column('database.upstr_db_isEmbedded', SqlType.bool()),
+                TableDefinition.Column('database.upstr_db_name', SqlType.varchar(max_length= 500)),
+                TableDefinition.Column('database.upstr_db_type', SqlType.varchar(max_length= 500)),
+                TableDefinition.Column('database_hostname', SqlType.varchar(max_length= 500)),
+ #               TableDefinition.Column('database_port', SqlType.varchar(max_length=500)),
+                TableDefinition.Column('EMB_DS_NAME', SqlType.varchar(max_length= 500)),
+                TableDefinition.Column('EMB_DS_REFRESH_TIME', SqlType.varchar(max_length=500)),
+                TableDefinition.Column('FIELD_ID', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('FIELD_NAME',SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('FORMULA', SqlType.text()),
+                TableDefinition.Column('OWNER.NAME',SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('site.name', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('UPSTR_SCHEMA', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+ #               TableDefinition.Column('TABLE_NAME', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('UPSTR_TABLE_ID', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('UPSTR_TABLE_NAME', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('VIEW_ID', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('VIEW_NAME', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('VIEW_TYPE', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('WORKBOOK_ID', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('WORKBOOK_NAME', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('WORKBOOK_PROJECT', SqlType.varchar(max_length= 500), NOT_NULLABLE),
+                TableDefinition.Column('WORKBOOK_URI', SqlType.varchar(max_length= 500), NOT_NULLABLE)           
              ])
+
 
     # Step 5: Create the table in the connection catalog
             connection.catalog.create_table(schema)
@@ -85,5 +89,3 @@ def create_hyper_file(conn, my_df, project_name):
 
 if __name__ == "__main__":
         create_hyper_file(conn, my_df, project_name)
-
-
